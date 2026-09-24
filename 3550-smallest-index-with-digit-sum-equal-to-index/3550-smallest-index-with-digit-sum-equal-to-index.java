f63fc590f1ef8ef093640bb6f71b7ddf85007d1e
chauhannirzara-svg <chauhannirzara@gmail.com>
2026-09-24 23:34:50 +0530

    Time: 1 ms (99.69%), Space: 45.6 MB (40.74%) - LeetHub

---
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int smallestIndex(int[] nums) {
+       
+        for(int i=0 ; i<nums.length ; i++){
+            int sum=0;
+            while(nums[i]!=0){
+                int rem = nums[i]%10;
+                sum+=rem;
+                nums[i]=(nums[i])/10;
+                         }
+            if(sum==i){
+                return i;
+            }  
+              
+
+        }
+        
+                return -1;
+                  
+        
+    }
+}
